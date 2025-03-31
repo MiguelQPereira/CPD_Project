26d70c8c26f7fa829d947b94bf979c66cb1508ef
miguel63064 <miguelferreira37954@gmail.com>
2025-03-31 19:22:32 +0100

    Poblema nao é nos primieros

--- MPI/parsim-mpi.c
+++ MPI/parsim-mpi.c
@@ -381,25 +381,26 @@ void cell_calculation(parcell* st_par, long grid_size, double space_size){
 
     printf("RANK: %d , Incoming prev: %d,Incoming next: %d,send prev: %d, next send %d \n",rank, incoming_prev_count, incoming_next_count,prev_count,next_count );
     if (incoming_prev_count > 0) {
-        //rcv_prev_par = malloc(incoming_prev_count * sizeof(particle_t));
-        //MPI_Irecv(rcv_prev_par, incoming_prev_count * sizeof(particle_t), MPI_PARTICLE_T, prev_rank, 3, MPI_COMM_WORLD, &recv_requests[0]);        
+        rcv_prev_par = malloc(incoming_prev_count * sizeof(particle_t));
+        MPI_Irecv(rcv_prev_par, incoming_prev_count * sizeof(particle_t), MPI_PARTICLE_T, prev_rank, 3, MPI_COMM_WORLD, &recv_requests[0]);        
     }
     if (incoming_next_count > 0) {
-        //rcv_next_par = malloc(incoming_next_count * sizeof(particle_t));
-        //MPI_Irecv(rcv_next_par, incoming_next_count * sizeof(particle_t), MPI_PARTICLE_T,next_rank, 2, MPI_COMM_WORLD, &recv_requests[1]);
+        rcv_next_par = malloc(incoming_next_count * sizeof(particle_t));
+        MPI_Irecv(rcv_next_par, incoming_next_count * sizeof(particle_t), MPI_PARTICLE_T,next_rank, 2, MPI_COMM_WORLD, &recv_requests[1]);
     }
     
     if (prev_count > 0) {
-        //MPI_Isend(to_send_prev.par, prev_count *sizeof(particle_t), MPI_PARTICLE_T, prev_rank, 2, MPI_COMM_WORLD, &send_requests[0]);
+        MPI_Isend(to_send_prev.par, prev_count *sizeof(particle_t), MPI_PARTICLE_T, prev_rank, 2, MPI_COMM_WORLD, &send_requests[0]);
     }
     
     if (next_count > 0) {
-        //MPI_Isend(to_send_next.par, next_count * sizeof(particle_t), MPI_PARTICLE_T, next_rank, 3, MPI_COMM_WORLD, &send_requests[1]);
+        MPI_Isend(to_send_next.par, next_count * sizeof(particle_t), MPI_PARTICLE_T, next_rank, 3, MPI_COMM_WORLD, &send_requests[1]);
     }
     
     if (incoming_prev_count > 0 || incoming_next_count > 0) {
-        //MPI_Waitall(2, recv_requests, statuses);
+        MPI_Waitall(2, recv_requests, statuses);
     }
+    /*
     //printf("Incoming: %d", incoming_prev_count);
     if (incoming_prev_count > 0) {
         for (int i = 0; i < incoming_prev_count; i++) {
@@ -450,7 +451,7 @@ void cell_calculation(parcell* st_par, long grid_size, double space_size){
     if (prev_count > 0 || next_count > 0) {
         //MPI_Waitall(2, send_requests, statuses);
     }
-
+    */
     free(rcv_next_par);
     free(to_send_next.par);
     free(to_send_prev.par);
