1789d91a2400be6517b371ac623e35393e653c1c
miguel63064 <miguelferreira37954@gmail.com>
2025-04-01 01:56:22 +0100

    a

--- MPI/parsim-mpi.c
+++ MPI/parsim-mpi.c
@@ -453,7 +453,7 @@ void cell_calculation(parcell* st_par, long grid_size, double space_size){
             double grid_y_aux = y / cell_size;
             int grid_y = (int)grid_y_aux;
         
-            int new_cell = (grid_x * grid_size + grid_y);
+            int new_cell = (grid_x * grid_size + grid_y) -start_point;
 
             printf("Rank %d: start point:%d final_point:%d new_cell = %d , npart %d, size: %d\n", rank,start_point, start_point+work_size[rank], new_cell, st_par[new_cell].n_particles, st_par[new_cell].size);
 
@@ -480,7 +480,7 @@ void cell_calculation(parcell* st_par, long grid_size, double space_size){
             double grid_y_aux = y / cell_size;
             int grid_y = (int)grid_y_aux;
         
-            int new_cell = (grid_x * grid_size + grid_y)- start_point;
+            int new_cell = (grid_x * grid_size + grid_y);
             
             st_par[new_cell].par[st_par[new_cell].n_particles] = rcv_next_par[i];
             st_par[new_cell].n_particles++;
