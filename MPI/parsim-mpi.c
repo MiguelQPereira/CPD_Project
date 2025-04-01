9b4fa0af314f967b8ac61db70d6be809bb33cbd4
miguel63064 <miguelferreira37954@gmail.com>
2025-04-01 15:24:21 +0100

    Update parsim-mpi.c

--- MPI/parsim-mpi.c
+++ MPI/parsim-mpi.c
@@ -308,7 +308,7 @@ void cell_calculation(parcell* st_par, long grid_size, double space_size){
                 //printf("Celula diferente \n");
                 
                 if ((rank == psize -1 && new_cell < 0 && new_cell <= 0 - work_size[rank-1]) ||
-                (rank != psize-1 && new_cell < 0) || (rank == 0 && new_cell >= work[rank+1]+work_size[rank])){
+                (rank != psize-1 && new_cell < 0) || (rank == 0 && new_cell >= work_size[rank+1]+work_size[rank])){
                     to_send_prev.par[to_send_prev.n_particles] = st_par[cell].par[id_par];
                     to_send_prev.n_particles ++;
                     
