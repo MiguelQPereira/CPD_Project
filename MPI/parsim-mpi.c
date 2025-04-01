f7c97a2c62e6a635c66c786400bff1cdd7572fe7
miguel63064 <miguelferreira37954@gmail.com>
2025-04-01 15:25:18 +0100

    Update parsim-mpi.c

--- MPI/parsim-mpi.c
+++ MPI/parsim-mpi.c
@@ -307,8 +307,8 @@ void cell_calculation(parcell* st_par, long grid_size, double space_size){
             if (new_cell != cell){
                 //printf("Celula diferente \n");
                 
-                if ((rank == psize -1 && new_cell < 0 && new_cell <= 0 - work_size[rank-1]) ||
-                (rank != psize-1 && new_cell < 0) || (rank == 0 && new_cell >= work_size[rank+1]+work_size[rank])){
+                if ((rank == psize -1 && new_cell < 0 && new_cell <= 0 - work_size[rank]) ||
+                (rank != psize-1 && new_cell < 0) || (rank == 0 && new_cell >= work_size[rank]+work_size[rank])){
                     to_send_prev.par[to_send_prev.n_particles] = st_par[cell].par[id_par];
                     to_send_prev.n_particles ++;
                     
@@ -319,7 +319,7 @@ void cell_calculation(parcell* st_par, long grid_size, double space_size){
                     }
 
                 }
-                else if ( (rank == psize -1 && new_cell <= -work_size[rank-2]- work_size[rank-1])  ||(rank != psize -1 && new_cell >= work_size[rank])){
+                else if ( (rank == psize -1 && new_cell <= -work_size[rank]- work_size[rank])  ||(rank != psize -1 && new_cell >= work_size[rank])){
                 
                     to_send_next.par[to_send_next.n_particles] = st_par[cell].par[id_par];
                     to_send_next.n_particles ++;
