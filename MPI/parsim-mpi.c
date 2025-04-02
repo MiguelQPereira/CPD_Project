f030124b8dc5329a3846a3d1136df21f231129e8
MiguelQPereira <miguelquinapereira@tecnico.ulisboa.pt>
2025-04-02 15:56:14 +0100

    merdadad

--- MPI/parsim-mpi.c
+++ MPI/parsim-mpi.c
@@ -734,7 +734,43 @@ int simulation(center_mass *cells, double space_size, long grid_size, long long 
         
         //printf("Antes Check colssions , Rank %d:\n", rank);
         //int repeated = 0;
-    
+
+        // Array to track valid collisions
+        bool *valid = malloc(collision_count * sizeof(bool));
+        for(int n=0; n < collision_count; n++) {
+            valid[n] = true;
+        }
+
+        for(int n=0; n < collision_count; n++) {
+            if (!valid[n]) continue;
+
+            int cell = colision[n].cell;
+            int a = colision[n].a;
+            int b = colision[n].b;
+
+            st_par[cell].par[a].alive = 0;
+            st_par[cell].par[b].alive = 0;
+
+            // Mark duplicates
+            for(int m = n+1; m < collision_count; m++) {
+                if (!valid[m]) continue;
+                if (colision[m].cell == cell && 
+                    (colision[m].a == a || colision[m].a == b || 
+                    colision[m].b == a || colision[m].b == b)) {
+                    valid[m] = false;
+                }
+            }
+        }
+
+        // Count valid collisions
+        int valid_count = 0;
+        for(int n=0; n < collision_count; n++) {
+            if (valid[n]) valid_count++;
+        }
+
+        free(valid);
+        // Use valid_count as needed
+        /*
         for(int n=0; n<collision_count; n++){
             st_par[colision[n].cell].par[colision[n].a].alive = 0;
             st_par[colision[n].cell].par[colision[n].b].alive = 0;
@@ -745,7 +781,7 @@ int simulation(center_mass *cells, double space_size, long grid_size, long long 
                     //continue;
                 }
             }
-        }
+        }*/
         //collision_count -= repeated;
 
         MPI_Barrier(MPI_COMM_WORLD);
