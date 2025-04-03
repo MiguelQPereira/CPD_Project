1f16e54cfa91cf2f8d3e1176f0284b6bdc07b853
MiguelQPereira <miguelquinapereira@tecnico.ulisboa.pt>
2025-04-03 19:39:29 +0100

    meda

--- MPI/parsim-mpi.c
+++ MPI/parsim-mpi.c
@@ -310,7 +310,7 @@ void cell_calculation(parcell* st_par, long grid_size, double space_size, int t)
                     for (int h=0; h<prev_rank; h++){
                         aux += work_size[h];
                     }
-                    if (new_cell+start_point < aux){// || new_cell+ start_point >= aux + work_size[prev_rank])
+                    if (new_cell+start_point < aux)// || new_cell+ start_point >= aux + work_size[prev_rank])
                         printf("--t=%d Rank:%d sending to rank %d particle in cell: %d\n", t, rank, prev_rank, new_cell);
                     
                     
