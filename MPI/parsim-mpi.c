0edd2c9b460b03c7dcb4e27cd366bfc86cc13a2a
miguel63064 <miguelferreira37954@gmail.com>
2025-04-03 20:22:53 +0100

    Update parsim-mpi.c

--- MPI/parsim-mpi.c
+++ MPI/parsim-mpi.c
@@ -305,11 +305,7 @@ void cell_calculation(parcell* st_par, long grid_size, double space_size, int t)
                     to_send_prev.par[to_send_prev.n_particles] = st_par[cell].par[id_par];
                     to_send_prev.n_particles ++;
 
-                    int aux;
-                    
-                    for (int h=0; h<prev_rank; h++){
-                        aux += work_size[h];
-                    }
+                    int aux = (grid_size* grid_size - work_size[prev_rank]);
                     printf("work_size=%d Rank:%d AUx %d New_cell %d\n", work_size[prev_rank], rank, aux, new_cell);
                     if(new_cell < aux || new_cell >= aux + work_size[prev_rank])
                         printf("--t=%d Rank:%d sending to rank %d particle in cell: %d\n", t, rank, prev_rank, new_cell);
