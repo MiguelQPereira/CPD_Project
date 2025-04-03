b88b4d5cc2e4e05cdb2d41a1d1cd3f42d7c1ad70
MiguelQPereira <miguelquinapereira@tecnico.ulisboa.pt>
2025-04-03 20:04:12 +0100

    mmm

--- MPI/parsim-mpi.c
+++ MPI/parsim-mpi.c
@@ -311,7 +311,7 @@ void cell_calculation(parcell* st_par, long grid_size, double space_size, int t)
                         aux += work_size[h];
                     }
                     
-                    if(new_cell + aux < aux || new_cell + aux >= aux + work_size[prev_rank])
+                    if(new_cell < aux || new_cell >= aux + work_size[prev_rank])
                         printf("--t=%d Rank:%d sending to rank %d particle in cell: %d\n", t, rank, prev_rank, new_cell);
                     //if (/*new_cell < aux ||*/ new_cell >= start_point)
                         //printf("--t=%d Rank:%d sending to rank %d particle in cell: %d\n", t, rank, prev_rank, new_cell);
